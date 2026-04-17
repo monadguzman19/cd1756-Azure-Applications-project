f686f5140a45e53d52914cb7d7d6e67aa63d37e6
monadguzman19 <Monalisa.DeGuzman@chevron.com>
2026-04-18 00:11:22 +0800

    Update models.py

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -38,6 +38,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
+    subtitle = db.Column(db.String(200))
     author = db.Column(db.String(75))
     body = db.Column(db.String(800))
     image_path = db.Column(db.String(100))
