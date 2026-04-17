fee90fc1b9a54279a7f6861c570a5816b375b8d4
monadguzman19 <Monalisa.DeGuzman@chevron.com>
2026-04-17 20:27:28 +0800

    Change logging level and stream handler settings
    Updated logging level to WARNING and adjusted stream handler.

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -3,20 +3,12 @@ The flask application package.
 """
 import logging
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_session import Session
 
 app = Flask(__name__)
-app.config.from_object(Config)
-app.logger.setLevel(logging.INFO)
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
-app.logger.addHandler(stream_handler)
-Session(app)
-db = SQLAlchemy(app)
-login = LoginManager(app)
-login.login_view = 'login'
+wsgi_app = app.wsgi_app
+app.logger.setLevel(logging.WARNING)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
 
 import FlaskWebProject.views
