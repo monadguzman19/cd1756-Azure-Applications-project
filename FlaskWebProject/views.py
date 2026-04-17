acae2f12b5fa1e6f5050a3c8f0f6d63c2d07cabf
monadguzman19 <Monalisa.DeGuzman@chevron.com>
2026-04-17 17:03:06 +0800

    Change redirect_uri to use HTTPS
    Update redirect_uri to use HTTPS scheme.

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -89,7 +89,7 @@ def authorized():
         result = msal_app.acquire_token_by_authorization_code (
             request.args['code'],
             scopes=Config.SCOPE,
-            redirect_uri = url_for("authorized", _external=True)
+            redirect_uri = url_for("authorized", _external=True, _scheme="https")
         )
         if "error" in result:
             return render_template("auth_error.html", result=result)
